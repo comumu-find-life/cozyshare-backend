ab6451c20bdba99d3f5214777aa3e599ed16ddb7
sinminseok12@naver.com <55183314+sinminseok@users.noreply.github.com>
2025-04-07 16:06:40 +0900

    test : CustomDirectMessage 테스트 코드 작성

--- fl-core/src/main/generated/com/core/mapper/ProtectedDealMapperImpl.java
+++ fl-core/src/main/generated/com/core/mapper/ProtectedDealMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-04-07T02:38:56+0900",
+    date = "2025-04-07T16:05:41+0900",
     comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.12.jar, environment: Java 17.0.6 (Amazon.com Inc.)"
 )
 @Component
--- fl-core/src/test/java/com/core/domain/chat/CustomDirectMessageRepositoryTest.java
+++ fl-core/src/test/java/com/core/domain/chat/CustomDirectMessageRepositoryTest.java
@@ -6,6 +6,8 @@ import com.core.domain.chat.model.DirectMessage;
 import com.core.domain.chat.repository.CustomDirectMessageRepository;
 import com.core.domain.chat.repository.DirectMessageRepository;
 import com.core.domain.chat.repository.impl.CustomDirectMessageRepositoryImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
@@ -18,11 +20,16 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static com.core.chat.entity_helper.DirectMessageHelper.generateDirectMessage;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @DataMongoTest
 @ActiveProfiles("test")
-@Import({CustomDirectMessageRepositoryImpl.class, EmbeddedMongoTestConfig.class})
+@Import({CustomDirectMessageRepositoryImpl.class})
 public class CustomDirectMessageRepositoryTest {
 
     @Autowired
@@ -31,21 +38,68 @@ public class CustomDirectMessageRepositoryTest {
     @Autowired
     private MongoTemplate mongoTemplate;
 
-    @Test
-    void testMongoConnection() {
-        System.out.println("Mongo DB name: " + mongoTemplate.getDb().getName());
+
+    @BeforeEach
+    void tearDown() {
+        mongoTemplate.getDb().drop();
     }
 
     @Test
-    void test(){
-        DirectMessage message1 = DirectMessage.builder()
-                .message("message1")
-                .receiverId(1L)
-                .senderId(2L)
-                .sentAt(LocalDateTime.now())
-                .isRead(false)
-                .build();
-        directMessageRepository.save(message1);
+    void 마지막_채팅_내역을_조회한다(){
+        //given
+        DirectMessage message = generateDirectMessage("message", 1L, 2L, LocalDateTime.now(), false);
+        directMessageRepository.save(message);
 
+        //when
+        Optional<DirectMessage> lastMessageByUserIds = directMessageRepository.findLastMessageByUserIds(1L, 2L);
+
+        //then
+        assertThat(lastMessageByUserIds.get().getMessage()).isEqualTo("message");
+    }
+
+    @Test
+    void 두사람의_채팅_내역을_모두_조회한다() {
+        //given
+        directMessageRepository.save(generateDirectMessage("message1", 1L, 2L, LocalDateTime.now(), false));
+        directMessageRepository.save(generateDirectMessage("message2", 2L, 1L, LocalDateTime.now(), false));
+        directMessageRepository.save(generateDirectMessage("message3", 1L, 2L, LocalDateTime.now(), false));
+
+        //when
+        List<DirectMessage> directMessageByUserIds1 = directMessageRepository.findDirectMessageByUserIds(1L, 2L);
+        List<DirectMessage> directMessageByUserIds2 = directMessageRepository.findDirectMessageByUserIds(2L, 1L);
+
+        //then
+        Assertions.assertThat(directMessageByUserIds1.size()).isEqualTo(3);
+        Assertions.assertThat(directMessageByUserIds2.size()).isEqualTo(3);
+    }
+
+
+    @Test
+    void 읽지않은_채팅내역_갯수를_조회한다() {
+        //given
+        directMessageRepository.save(generateDirectMessage("message1", 1L, 2L, LocalDateTime.now(), false));
+        directMessageRepository.save(generateDirectMessage("message2", 1L, 2L, LocalDateTime.now(), false));
+        directMessageRepository.save(generateDirectMessage("message3", 1L, 2L, LocalDateTime.now(), false));
+
+        //when
+        int response = directMessageRepository.countNotReadMessage(1L, 2L);
+
+        //then
+        Assertions.assertThat(response).isEqualTo(3);
+    }
+
+    @Test
+    void 읽지않은_채팅내역을_읽음_상태로_변경한다() {
+        //given
+        directMessageRepository.save(generateDirectMessage("message1", 1L, 2L, LocalDateTime.now(), false));
+        directMessageRepository.save(generateDirectMessage("message2", 1L, 2L, LocalDateTime.now(), false));
+        directMessageRepository.save(generateDirectMessage("message3", 1L, 2L, LocalDateTime.now(), false));
+
+        //when
+        directMessageRepository.markMessagesAsRead(1L, 2L);
+        int response = directMessageRepository.countNotReadMessage(1L, 2L);
+
+        //then
+        Assertions.assertThat(response).isEqualTo(0);
     }
 }
