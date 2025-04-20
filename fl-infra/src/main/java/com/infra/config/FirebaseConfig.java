ea6d24f7d09700d19ebaa405b75255a31256d3c4
sinminseok12@naver.com <55183314+sinminseok@users.noreply.github.com>
2025-04-20 20:47:11 +0900

    refactor : fireabse json 경로 환경변수로 설정

--- fl-infra/src/main/java/com/infra/config/FirebaseConfig.java
+++ fl-infra/src/main/java/com/infra/config/FirebaseConfig.java
@@ -18,23 +18,42 @@ public class FirebaseConfig {
     @Value(("${firebase.project-id}"))
     private String projectId;
 
-    @Value(("${firebase.key-name}"))
-    private String keyName;
+    @Value(("${firebase.key-url}"))
+    private String keyPath;
 
     @Bean
-    FirebaseMessaging firebaseMessaging() throws IOException {
-        ClassPathResource resource = new ClassPathResource(keyName);
-        InputStream refreshToken = resource.getInputStream();
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(refreshToken))
-                .setProjectId(projectId)
-                .build();
-        FirebaseApp app;
-        if (FirebaseApp.getApps().isEmpty()) {
-            app = FirebaseApp.initializeApp(options);
-        } else {
-            app = FirebaseApp.getInstance();
+    public FirebaseMessaging firebaseMessaging() throws IOException {
+        try (FileInputStream serviceAccount = new FileInputStream(keyPath)) {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setProjectId(projectId)
+                    .build();
+
+            FirebaseApp app;
+            if (FirebaseApp.getApps().isEmpty()) {
+                app = FirebaseApp.initializeApp(options);
+            } else {
+                app = FirebaseApp.getInstance();
+            }
+
+            return FirebaseMessaging.getInstance(app);
         }
-        return FirebaseMessaging.getInstance(app);
     }
+
+//    @Bean
+//    FirebaseMessaging firebaseMessaging() throws IOException {
+//        ClassPathResource resource = new ClassPathResource(keyName);
+//        InputStream refreshToken = resource.getInputStream();
+//        FirebaseOptions options = FirebaseOptions.builder()
+//                .setCredentials(GoogleCredentials.fromStream(refreshToken))
+//                .setProjectId(projectId)
+//                .build();
+//        FirebaseApp app;
+//        if (FirebaseApp.getApps().isEmpty()) {
+//            app = FirebaseApp.initializeApp(options);
+//        } else {
+//            app = FirebaseApp.getInstance();
+//        }
+//        return FirebaseMessaging.getInstance(app);
+//    }
 }
